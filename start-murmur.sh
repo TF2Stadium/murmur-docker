de78bdfaf1394b66d76dff3e8a9383028131f0d2
George Pittarelli <george.pittarelli@massdrop.com>
2016-11-04 22:32:30 -0700

    conditionally setup murmur SSL cert/key

--- start-murmur.sh
+++ start-murmur.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# triply sure there's a newline on the config:
+echo "" >> /etc/murmur.ini
+
+[ -f /data/fullchain.pem ] && { echo "sslCert=/data/fullchain.pem" >> /etc/murmur.ini ; }
+[ -f /data/privkey.pem ] && { echo "sslKey=/data/privkey.pem" >> /etc/murmur.ini ; }
+
 /opt/murmur/murmur.x86 -supw $MURMUR_PASSWORD
 /opt/murmur/murmur.x86 -fg -v -ini /etc/murmur.ini
